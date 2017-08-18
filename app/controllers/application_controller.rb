5783b81fed2680c43dab58767186418c0c7d6390
Timothee Reynaud <reynaudtimothee@gmail.com>
2017-08-18 15:03:40 +0200

    added attributes to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
