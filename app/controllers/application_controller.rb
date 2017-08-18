fe80b959db263df6e492c3ab86fad6cd562eb2ed
Timothee Reynaud <reynaudtimothee@gmail.com>
2017-08-18 17:23:14 +0200

    edition de profile en séparant la partie info (modifier un password et l'email) et la partie profile (adresse, zip code, description), creation d'un controller pour le profile (plus en lien avec devise)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :zip_code,
-                                                              :city, :description])
+
   end
+
+  # trying to redirect user to current page after log-in
+  # def after_sign_in_path_for(resource)
+  # request.referrer
+  # end
 end
---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,24 @@
+class ProfileController < ApplicationController
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:account_update, keys: [:address, :zip_code, :city, :description])
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :zip_code, :city, :description)
+  end
+end
+
+
